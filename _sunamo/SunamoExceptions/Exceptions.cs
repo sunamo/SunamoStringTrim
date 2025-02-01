b20ab0460051192df8799d3bfabded9134b196f8
sunamo.cz <radek.jancik@sunamo.cz>
2025-02-01 08:45:20 +0100

    Added a better self-descriptive message

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -4,26 +4,6 @@ internal sealed partial class Exceptions
 {
     #region Other
 
-internal static void TypeAndMethodName(string l, out string type, out string methodName)
-    {
-        var s2 = l.Split("at ")[1].Trim();
-        var s = s2.Split("(")[0];
-        var p = s.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        methodName = p[^1];
-        p.RemoveAt(p.Count - 1);
-        type = string.Join(".", p);
-    }
-    internal static string CallingMethod(int v = 1)
-    {
-        StackTrace stackTrace = new();
-        var methodBase = stackTrace.GetFrame(v)?.GetMethod();
-        if (methodBase == null)
-        {
-            return "Method name cannot be get";
-        }
-        var methodName = methodBase.Name;
-        return methodName;
-    }
     #endregion
 
     #region IsNullOrWhitespace
